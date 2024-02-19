8356a53d84636d4d9f1c8f3d7818ac92d35a026d
kunchtler <leo.kulinski@gmail.com>
2024-02-20 00:46:46 +0100

    Typed utils with Sequence instead of List

--- src/musicaljuggling/automata/utils.py
+++ src/musicaljuggling/automata/utils.py
@@ -1,36 +1,36 @@
-from typing import TypeVar, Iterator
+from typing import TypeVar
 from collections.abc import Sequence
 
 T = TypeVar("T")
 
 
-def stringify(iter: Iterator[T]) -> str:
-    return "".join(str(elem) for elem in iter)
+def stringify(seq: Sequence[T]) -> str:
+    return "".join(str(elem) for elem in seq)
 
 
-def cyclic_left_shift(list_: list[T], shift_by: int = 1) -> list[T]:
-    return [list_[(i + shift_by) % len(list_)] for i in range(len(list_))]
+def cyclic_left_shift(seq: Sequence[T], shift_by: int = 1) -> list[T]:
+    return [seq[(i + shift_by) % len(seq)] for i in range(len(seq))]
 
 
-def cyclic_right_shift(list_: list[T], shift_by: int = 1) -> list[T]:
-    return [list_[(i - shift_by) % len(list_)] for i in range(len(list_))]
+def cyclic_right_shift(seq: Sequence[T], shift_by: int = 1) -> list[T]:
+    return [seq[(i - shift_by) % len(seq)] for i in range(len(seq))]
 
 
-def left_shift(list_: list[T], shift_by: int = 1) -> list[T]:
-    if len(list_) == 0:
+def left_shift(seq: Sequence[T], shift_by: int = 1) -> list[T]:
+    if len(seq) == 0:
         return []
-    typ = type(list_[0])
+    typ = type(seq[0])
     return [
-        list_[(i + shift_by)] if i + shift_by < len(list_) else typ()
-        for i in range(len(list_))
+        seq[(i + shift_by)] if i + shift_by < len(seq) else typ()
+        for i in range(len(seq))
     ]
 
 
-def right_shift(list_: list[T], shift_by: int = 1) -> list[T]:
-    if len(list_) == 0:
+def right_shift(seq: Sequence[T], shift_by: int = 1) -> list[T]:
+    if len(seq) == 0:
         return []
-    typ = type(list_[0])
-    return [typ() if i < shift_by else list_[i - shift_by] for i in range(len(list_))]
+    typ = type(seq[0])
+    return [typ() if i < shift_by else seq[i - shift_by] for i in range(len(seq))]
 
 
 def find_indices(seq: Sequence[T], elem_to_find: T) -> list[int]:
